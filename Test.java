41cb39537f46cbfe8c5be9d72c9341b663580821
harshapradasonawane-art <harshapradasonawane@gmail.com>
2026-01-19 17:43:31 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,52 @@
+package com.tka.janA1;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		
+		Player p1 = new Player(1,7,"MSD",5439,0,"CSK");
+		
+		Player p2= new Player(2,8,"jadeja",3260,170,"CSK");
+		
+		Player p3 = new Player(3,11,"sanju",4700,0,"CSK");
+		
+		Player p4 = new Player(4,3,"dewald brewis",455,1,"CSK");
+		
+		Player p5 = new Player(5,30,"dube",1800,5,"CSK");
+		
+		Player p6 = new Player(6,97,"sarfarz",585,0,"CSK");
+		
+		Player p7 = new Player(7,56,"ayush mahtre",94,0,"CSK");
+		
+		Player p8 = new Player(8,15,"noor",20,48,"CSK");
+		
+		Player p9 = new Player(9,37,"urvil",68,0,"CSK");
+		
+		Player p10 = new Player(10,47,"anushul",286,10,"CSK");
+		
+		Player p11 = new Player(11,49,"mukesh",32,36,"CSK");
+		
+		
+		System.out.println(p1.toString());
+		
+		System.out.println(p2.toString());
+		
+		System.out.println(p3.toString());
+		
+		System.out.println(p4.toString());
+		
+		System.out.println(p5.toString());
+		
+		System.out.println(p6.toString());
+		
+		System.out.println(p7.toString());
+		
+		System.out.println(p8.toString());
+		
+		System.out.println(p9.toString());
+		
+		System.out.println(p10.toString());
+		
+		System.out.println(p11.toString());
+	}
+}
