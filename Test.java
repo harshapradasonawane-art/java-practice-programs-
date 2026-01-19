7503f6e1026dff39427639dcb477ed46dd2fb86f
harshapradasonawane-art <harshapradasonawane@gmail.com>
2026-01-19 17:52:11 +0530

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,52 +1,32 @@
-package com.tka.janA1;
+package com.tka.jan18;
 
 public class Test {
+
 	public static void main(String[] args) {
 		
-		
-		Player p1 = new Player(1,7,"MSD",5439,0,"CSK");
-		
-		Player p2= new Player(2,8,"jadeja",3260,170,"CSK");
-		
-		Player p3 = new Player(3,11,"sanju",4700,0,"CSK");
-		
-		Player p4 = new Player(4,3,"dewald brewis",455,1,"CSK");
-		
-		Player p5 = new Player(5,30,"dube",1800,5,"CSK");
-		
-		Player p6 = new Player(6,97,"sarfarz",585,0,"CSK");
-		
-		Player p7 = new Player(7,56,"ayush mahtre",94,0,"CSK");
-		
-		Player p8 = new Player(8,15,"noor",20,48,"CSK");
-		
-		Player p9 = new Player(9,37,"urvil",68,0,"CSK");
-		
-		Player p10 = new Player(10,47,"anushul",286,10,"CSK");
-		
-		Player p11 = new Player(11,49,"mukesh",32,36,"CSK");
+
+		Product p1 = new Product(101,"mobile",15000,"electronics","2077",10,"samsung","black","samsung",0.5);
+		Product p2 = new Product(102,"laptop",55000,"electronics","2080",5,"dell","grey","dell",2.3);
+		Product p3 = new Product(103,"tv",45000,"electronics","2029",3,"sony","black","sony",8.5);
+		Product p4 = new Product(104,"fridge",30000,"electronics","2026",2,"LG","silaver","LG",55);
+		Product p5 = new Product(105,"washing machin",28000,"electronics","2030",4,"IFB","white","IFB",60);
+		Product p6 = new Product(106,"mixer",3500,"kitchen","2026",8,"philips","blue","philips",3);
+		Product p7 = new Product(107,"camera",90000,"electronics","2031",12,"sony","black","sony",1.2);
+		Product p8 = new Product(108,"ac",4200,"electronics","2031",2,"voltas","white","voltas",45);
+		Product p9 = new Product(109,"fan",2500,"electronics","2028",15,"usha","brown","usha",4);
+		Product p10 = new Product(110,"headphone",22000,"electronics","2027",10,"boat","black","boat",0.3);
 		
 		
-		System.out.println(p1.toString());
-		
-		System.out.println(p2.toString());
-		
-		System.out.println(p3.toString());
-		
-		System.out.println(p4.toString());
-		
-		System.out.println(p5.toString());
-		
-		System.out.println(p6.toString());
-		
-		System.out.println(p7.toString());
-		
-		System.out.println(p8.toString());
-		
-		System.out.println(p9.toString());
-		
-		System.out.println(p10.toString());
-		
-		System.out.println(p11.toString());
+		System.out.println(p1);		
+		System.out.println(p2);
+		System.out.println(p3);
+		System.out.println(p4);
+		System.out.println(p5);
+		System.out.println(p6);
+		System.out.println(p7);
+		System.out.println(p8);
+		System.out.println(p9);
+		System.out.println(p10);
 	}
+
 }
